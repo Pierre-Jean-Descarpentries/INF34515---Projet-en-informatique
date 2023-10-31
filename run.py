37a3157461e65e76c28dd805053d22b5be4f9873
Pierre-Jean <pierre-jean.descarpentries@epitech.eu>
2023-10-31 02:12:36 +0100

    [+] Added command line interpretor to use methods in run.py

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 #!/bin/python3
 
 import sys
+import time
+import EnergyDataHarvest
+
+## Run command until stop asked (ctrl+c)
+def realTime():
+    while True:
+        EnergyDataHarvest.getNRGData()
+        time.sleep(300)
 
 def main() -> int:
+    command = input("> ")
+
+    if (command.lower() == "run realtime"):
+        realTime()
+    elif (command.lower() == "run interupt"):
+        EnergyDataHarvest.getInterrupt()
     return (0)
 
 if (__name__ == '__main__'):
