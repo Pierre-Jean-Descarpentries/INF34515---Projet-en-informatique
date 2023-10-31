cba345f690d21f768a427dadd4db59e737c6a2ad
Pierre-Jean <pierre-jean.descarpentries@epitech.eu>
2023-10-31 02:15:13 +0100

    [+] Added getInterrupt method to get the informations of the outages from the website

--- EnergyDataHarvest.py
+++ EnergyDataHarvest.py
@@ -6,25 +6,50 @@ import numpy as np
 from bs4 import BeautifulSoup
 
 def getNRGData() -> int:
-    r = requests.get('https://tso.nbpower.com/Public/fr/SystemInformation_realtime.asp')
-    htmlParser = BeautifulSoup(r.content, 'html.parser')
-    txt = htmlParser.get_text().split('\n')
-    data = []
-    dataName = ["Charge au NB",
-                "Demande au NB",
-                "ISO-NE",
-                "EMEC",
-                "MPS",
-                "QUEBEC",
-                "NOVA SCOTIA",
-                "PEI",
-                "Marge de réserve d'exploitation non synchrone de 10 min",
-                "Marge de réserve d'exploitation synchrone de 10 min",
-                "Marge de réserve d'exploitation de 30 min"]
-    for x in range(45, 55):
-        data = np.append(data, [dataName[x - 45], float(txt[x])])
+    try:
+        r = requests.get("https://tso.nbpower.com/Public/fr/SystemInformation_realtime.asp")
+        htmlParser = BeautifulSoup(r.content, "html.parser")
+        txt = htmlParser.get_text().split('\n')
+        data = []
+        dataName = ["Charge au NB",
+                    "Demande au NB",
+                    "ISO-NE",
+                    "EMEC",
+                    "MPS",
+                    "QUEBEC",
+                    "NOVA SCOTIA",
+                    "PEI",
+                    "Marge de réserve d'exploitation non synchrone de 10 min",
+                    "Marge de réserve d'exploitation synchrone de 10 min",
+                    "Marge de réserve d'exploitation de 30 min"]
+        for x in range(45, 56):
+            data = np.append(data, [dataName[x - 45], float(txt[x])])
 
-    print(data)
-    return (0)
+        print(data)
+        return (0)
+    except Exception:
+        print(Exception, file=sys.stderr)
+        return (84)
 
-sys.exit(getNRGData())
+### DATA VALUES (heure d'Atlentique)
+### [Numéro d'intéruption, Status, Heure de début, Heure d'arrêt, but]
+def getInterrupt() -> int:
+    try:
+        r = requests.get("https://tso.nbpower.com/Public/fr/outages.aspx")
+        htmlParser = BeautifulSoup(r.content, "html.parser")
+        txt = htmlParser.table.get_text().split('\n')
+        while ("" in txt):
+            txt.remove("")
+        data = []
+
+        for x in range(5, len(txt), 5):
+            while (x + 5 < len(txt) and txt[x + 5].isnumeric() == False):
+                txt[x + 4] += "\n" + txt[x + 5]
+                txt[x + 5] = ""
+                txt.remove("")
+            data.insert(0, txt[x:x + 5])
+        data.remove([])
+        return (0)
+    except Exception:
+        print(Exception, file=sys.stderr)
+        return (84)
